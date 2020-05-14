93071c218e2eba4958ea1198122cbf8f6ae504c5
gelezhinis <gelezhinis@gmail.com>
2020-05-14 19:15:25 +0300

    enabling sticky navbar

--- js/main.js
+++ js/main.js
@@ -71,13 +71,13 @@ function navigate(lat, lng) {
 }
 
 // Sticky Navbar
-// window.addEventListener('scroll', function() {
-//    if(window.scrollY > 85) {
-//       this.document.querySelector('#navbar').style.opacity = 0.9;
-//    } else {
-//       this.document.querySelector('#navbar').style.opacity = 1;
-//    }
-// });
+window.addEventListener('scroll', function() {
+   if(window.scrollY > 85) {
+      this.document.querySelector('#navbar').style.opacity = 0.9;
+   } else {
+      this.document.querySelector('#navbar').style.opacity = 1;
+   }
+});
 
 // Smooth Scrolling
 $('#navbar a, .btn').on('click', function(event) {
